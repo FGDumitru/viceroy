d1c2279d9b173e93425cc89cc23fcfb138626ae6
Florin Dumitru <81620182+FGDumitru@users.noreply.github.com>
2024-07-17 12:27:10 +0300

    Improved tokenizer demo.

--- examples/tokenizer.php
+++ examples/tokenizer.php
@@ -8,6 +8,8 @@ $text = <<<TEXT
 Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
 TEXT;
 
+echo "\nOriginal text:\n\t$text\n";
+
 $llmConnection = new llamacppOAICompatibleConnection();
 
 // Check the endpoint health.
@@ -21,6 +23,15 @@ if ($tokens) {
   $tokensCount = count($tokens);
 
   echo "Text tokenized to: $tokensCount tokens.";
+  
+  $detokenizedString = $llmConnection->detokenize($tokens);
+  
+  if ($detokenizedString) {
+    echo "\nTokens detokenized back to:\n\t$detokenizedString\n";
+  } else {
+    echo "\nFailed to detokenized the tokens list.\n";
+  }
+  
 } else {
-  echo "\nFailed to generate the tokens lost.\n";
+  echo "\nFailed to generate the tokens list.\n";
 }
